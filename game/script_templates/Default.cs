4fa857d5263de81c9f6447f0ae9426b6ed774af5
Marquis Kurt <software@marquiskurt.net>
2021-08-06 11:01:00 -0400

    :truck: Add scripting template

---
+++ game/script_templates/Default.cs
@@ -0,0 +1,26 @@
+// %CLASS%
+// (C) 2021 Marquis Kurt.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using Godot;
+using System;
+
+namespace PackageDelivered
+{
+    /// <summary>Documentation for <c>%CLASS%</c> goes here.</summary>
+    public class %CLASS% : %BASE%
+    {
+        // Declare member variables here. Examples:
+        // private int a = 2;
+        // private string b = "text";
+
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+
+        }
+    }
+}
