435cc282fc49179a424cd51699bd6069972825b3
Marquis Kurt <software@marquiskurt.net>
2022-03-10 12:31:22 -0500

    :memo: Add ITeardownable documentation
    ^SPR-3 work Documentation 1m
    
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- game/scripts/Logic/ITeardownable.cs
+++ game/scripts/Logic/ITeardownable.cs
@@ -2,8 +2,15 @@ using Godot;
 
 namespace PackageResolved.Logic
 {
+    /// <summary>
+    /// An interface that indicates that the class has a teardown function that should be called when being 
+    /// deinitialized from the scene tree.
+    /// </summary>
     public interface ITeardownable
     {
+        /// <summary>
+        /// Tears down the class to either be freed from memory or be marked as ready for reinitialization.
+        /// </summary>
         void Teardown();
     }
 }
