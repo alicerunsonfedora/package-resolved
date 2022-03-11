25adacdc7eff6c08bf57c1e8318ddfc5c6ba526e
Marquis Kurt <software@marquiskurt.net>
2022-03-10 21:16:56 -0500

    :memo: Add GameOver documentation
    ^SPR work Documentation 1m
    
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- game/scripts/UI/GameOver.cs
+++ game/scripts/UI/GameOver.cs
@@ -2,12 +2,24 @@ using Godot;
 
 namespace PackageResolved.UI
 {
+    /// <summary>
+    /// A class that manages the user interface for the game over screen.
+    /// </summary>
     public class GameOver : Control
     {
+        /// <summary>
+        /// The button that causes the game to restart when clicked.
+        /// </summary>
         public Button BtnRestart;
+
+        /// <summary>
+        /// The button that allows the player to return to the main menu.
+        /// </summary>
         public Button BtnQuitToMenu;
 
-
+        /// <summary>
+        /// Instantiate the scene after entering the scene tree.
+        /// </summary>
         public override void _Ready()
         {
             BtnRestart = GetNode<Button>("VBoxContainer/Restart");
@@ -17,11 +29,17 @@ namespace PackageResolved.UI
             BtnQuitToMenu.Connect("button_up", this, "BtnPressQuitToMenu");
         }
 
+        /// <summary>
+        /// A callback method called when the user clicks on the restart button.
+        /// </summary>
         private void BtnPressRestart()
         {
             GetTree().ChangeScene("res://scenes/game_loop.tscn");
         }
 
+        /// <summary>
+        /// A callback method called when the user clicks on the main menu button.
+        /// </summary>
         private void BtnPressQuitToMenu()
         {
             GetTree().ChangeScene("res://scenes/main_menu.tscn");
