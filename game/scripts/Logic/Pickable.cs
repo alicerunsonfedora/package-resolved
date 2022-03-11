48a4cdc47039c6db913cdd406bf9172bee2a4742
Marquis Kurt <software@marquiskurt.net>
2022-03-11 12:08:04 -0500

    :bulb: Add missing copyright headers

--- game/scripts/Logic/Pickable.cs
+++ game/scripts/Logic/Pickable.cs
@@ -1,3 +1,11 @@
+// Pickable.cs
+// Package Resolved
+//
+// (C) 2021-2022 Marquis Kurt.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 using Godot;
 
 namespace PackageResolved.Logic
